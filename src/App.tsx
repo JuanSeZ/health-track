fb02008fc8cf411307338afceb3870afcb2827b3
franciscocavallaro <ccavallaro00@gmail.com>
2024-11-12 21:59:44 -0300

    Conflicts solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import './App.css';
-import Card from './components/card';
-import PlusIcon from './assests/plus-icon.tsx'
+import Navbar from './components/navbar/index.tsx';
 
 function App() {
   return (
     <div>
-      {/*<Navbar />*/}
-      <Card
-        variant="default"
-        description='This is a card component'
-        backgroundColor="primary-100"
-        icon={<PlusIcon color="#663400" />}
-        iconBackgroundColor={'primary-400'}
-        headerColor={'primary-400'}
-        buttonVariant='secondaryOutlined'
-      />
+      <Navbar />
     </div>
   );
 }
